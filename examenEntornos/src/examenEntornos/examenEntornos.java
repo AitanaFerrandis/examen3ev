41b7463026b0c2cc37aa8745ba0de8dbf154be54
Paco Gomez <fjgomez@florida-uni.es>
2016-05-23 15:35:35 +0200

    TOMA ya

--- examenEntornos/src/examenEntornos/examenEntornos.java
+++ examenEntornos/src/examenEntornos/examenEntornos.java
@@ -3,7 +3,7 @@ package examenEntornos;
 public class examenEntornos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int i=0;i<10000;i++) System.out.println("PESADOS DE MIERDA");
 
 	}
 
