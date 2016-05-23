192b4bd537d38e243dc6e876ddfc86270bfb4c51
Aitana <Aitana@10.10.2.205>
2016-05-23 15:38:20 +0200

    Cambio syso del for

--- examenEntornos/src/examenEntornos/examenEntornos.java
+++ examenEntornos/src/examenEntornos/examenEntornos.java
@@ -3,7 +3,7 @@ package examenEntornos;
 public class examenEntornos {
 
 	public static void main(String[] args) {
-		for(int i=0;i<10000;i++) System.out.println("PESADOS DE MIERDA");
+		for(int i=0;i<10000;i++) System.out.println("QUE AMOR DE ALUMNOS");
 
 	}
 
